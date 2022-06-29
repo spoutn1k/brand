90090dd199e28acee35471a08e24ccbe281d7198
Jean-Baptiste Skutnik <jb.skutnik@gmail.com>
2022-06-28 22:00:16 -0700

    Update

--- resize.py
+++ resize.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import sys
 import logging
 from shutil import which
 from shlex import split
@@ -8,30 +9,34 @@ from subprocess import Popen, PIPE
 from pathlib import Path
 
 
-def gen_converts(imagefile: Path, processes: list[Popen]):
+def compress_clean(imagefile: Path, processes: list[Popen]):
+    magick_tool = which('convert')
+
+    convert_tiff = f"""{magick_tool} {imagefile.resolve()}[0] -compress lzw "{imagefile.with_suffix('.tiff')}" """
+
+    logging.debug("TIFF: %s", split(convert_tiff))
+
+    processes.append(Popen(split(convert_tiff), stdout=PIPE, stderr=PIPE))
+
+
+def create_thumb(imagefile: Path, processes: list[Popen]):
     magick_tool = which('convert')
     image = Image.open(imagefile)
     width, height = image.size
 
-    convert_tiff = f"""{magick_tool} {imagefile.resolve()}[0] -compress lzw "{imagefile.with_suffix('.tiff')}" """
     convert_jpg = f"""{magick_tool} {imagefile.resolve()}[0] -resize "{width//2}"x"{height//2}" "{imagefile.with_suffix('.jpg')}" """
 
-    logging.debug("TIFF: %s", split(convert_tiff))
     logging.debug("JPG: %s", split(convert_jpg))
 
-    processes.append(Popen(split(convert_tiff), stdout=PIPE, stderr=PIPE))
     processes.append(Popen(split(convert_jpg), stdout=PIPE, stderr=PIPE))
 
 
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-
+def apply_transformation(reg, transform):
     conversions = []
 
-    import sys
-    for tif in Path(sys.argv[1]).glob('*tif'):
-        logging.debug("Converting '%s'", tif)
-        gen_converts(tif, conversions)
+    for file in directory.glob(reg):
+        logging.debug("Converting '%s'", file)
+        transform(file, conversions)
 
     for proc in conversions:
         status = proc.wait()
@@ -39,3 +44,11 @@ if __name__ == '__main__':
         if status:
             logging.critical(f"Process %d failed with error code %d: %s",
                              proc.pid, proc.returncode, proc.stderr)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    directory = Path(sys.argv[1])
+
+    apply_transformation('*tif', compress_clean)
+    apply_transformation('*tiff', create_thumb)
